10469041a0445102196079fdce953208d8c779ba
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-02-23 15:23:15 +0200

    initial commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
 ]
 
 urlpatterns = [
-    path('book/add/', views.add_book, name='add_book'),
-    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('book/add/', views.add_book, name='add_book/'),
+    path('book/<int:pk>/edit/', views.edit_book, name='edit_book/'),
     path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
