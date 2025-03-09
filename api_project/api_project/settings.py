1983d4a38e10e691c192fe8c6f98a18ab22228fc
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-03-09 15:26:21 +0200

    initial commit

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
+rest_framework.permissions.IsAuthenticated
 """
 
 from pathlib import Path
