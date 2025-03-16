1fc452ad3f79582210aa03f98a23bd3077b83239
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-03-16 21:51:47 +0200

    initial commit

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class TestView(APIView):
+    def get(self, request):
+        return Response({"message": "Django REST Framework is working!"}, status=status.HTTP_200_OK)
