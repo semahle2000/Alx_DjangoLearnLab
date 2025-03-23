2bdb67100e3a46847bb5d86759184cbe2b585788
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-03-23 22:53:39 +0200

    Integrate Tagging Functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', related_name='posts')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -19,3 +27,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+    
