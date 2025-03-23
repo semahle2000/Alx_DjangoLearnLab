b41ab577d8e610ffa1d7e78cf526e93cf360be6a
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-03-23 21:16:16 +0200

    modify blog/urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 ]
