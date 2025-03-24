29c63a077a91270434fed0d20a70e2e59631c2e7
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-03-24 03:33:43 +0200

    Edit the forms.py file

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 from .models import Post, Comment, Tag
-from .widgets import TagWidget  # Import the custom widget
+from .widgets import TagWidget  # Import the custom widget TagWidget().
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
