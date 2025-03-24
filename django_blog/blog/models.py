6c21a34eb8bd3508b5068721a937dd8fd4bf1da8
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-03-24 03:10:02 +0200

    modify files

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,20 +6,20 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    tags = models.ManyToManyField('Tag', related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True) #published_date is renamed to created_at
+    created_at = models.DateTimeField(auto_now_add=True)  # published_date is renamed to created_at
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
+
 class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -27,4 +27,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
-    
