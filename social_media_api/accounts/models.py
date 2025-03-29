98720837d7f35a930ee461ab3643d265c9701e1e
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-03-29 08:35:24 +0200

    Setup Project and User Authentication for a Social Media API

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+    
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',  
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_set',  
+        blank=True
+    )
